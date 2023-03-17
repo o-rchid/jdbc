796d150bd0bd64bc6c613ad7d3efb6eda5f99f9d
o-rchid <ozzve35@gmail.com>
2023-03-17 23:54:28 +0900

    자바 예외 이해 : 예외 포함과 스택 트레이스

--- src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,11 +1,13 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
 import java.sql.SQLException;
 
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
@@ -13,6 +15,18 @@ public class UnCheckedAppTest {
         Controller controller = new Controller();
         Assertions.assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(Exception.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            //e.printStackTrace();
+            log.info("ex", e);
+        }
+
     }
 
     static class Controller {
@@ -44,7 +58,7 @@ public class UnCheckedAppTest {
             try {
                 runSQL();
             } catch (SQLException e) {
-                throw new RuntimeException(e);
+                throw new RuntimeSQLException(e);
             }
         }
 
